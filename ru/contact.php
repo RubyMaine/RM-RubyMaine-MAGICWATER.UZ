6ec93954ad1d2f890875847ea8107280f47ca89f
RubyMaine <104354155+RubyMaine@users.noreply.github.com>
2022-09-29 10:21:43 +0500

    RM-RubyMaine-MAGICWATER.UZ

---
+++ ru/contact.php
@@ -0,0 +1,288 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <title> MAGICWATER® ~ НАШ ПРОДУКТЫ</title>
+
+    <!-- :::::::::::::: FAVICON ICON ::::::::::::::-->
+    <link rel="shortcut icon" href="assets/images/logo/favicon.png" type="image/png">
+
+    <!-- :::::::::::::: All CSS FILES :::::::::::::: -->
+    <link rel="stylesheet" href="assets/css/vendor/vendor.min.css">
+    <link rel="stylesheet" href="assets/css/plugins/plugins.min.css">
+    <link rel="stylesheet" href="assets/css/style.min.css">
+
+</head>
+
+<body>
+
+<!-- ...:::: HEADER MENU :::... -->
+    <header class="header-section d-none d-xl-block">
+        <div class="header-wrapper">
+            <div class="header-bottom header-bottom-color--black section-fluid sticky-header sticky-color--black">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-12 d-flex align-items-center justify-content-between">
+
+                            <div class="header-logo">
+                                <div class="logo">
+                                    <a href="https://magicwater.uz/ru/"><img src="assets/images/logo/logo.png" alt=""></a>
+                                </div>
+                            </div>
+
+
+                            <div class="main-menu menu-color--white menu-hover-color--pink">
+                                <nav>
+                                    <ul>
+                                        <li class="has-dropdown"><a href="https://magicwater.uz/ru/" class="active main-menu-link" > ГЛАВНАЯ СТРАНИЦА </a></li>
+                                        <li class="has-dropdown has-megaitem"><a href="https://magicwater.uz/ru/shop.php"> НАШ ПРОДУКТЫ </a></li>
+                                        <li><a href="https://magicwater.uz/ru/contact.php"> СВЯЗАТЬСЯ С НАМИ </a></li>
+                                    </ul>
+                                </nav>
+                            </div>
+
+
+                            <ul class="header-action-link action-color--white action-hover-color--pink">
+                                <li><a href="https://www.facebook.com/magicuzb"><i class="fa fa-facebook"></i></a></li>
+                                <li><a href="https://www.instagram.com/magicwater_uz/"><i class="fa fa-instagram"></i></a></li>
+                                <li><a href="https://t.me/magicwater_uz"><i class="fa fa-telegram"></i></a></li>
+                                <li><a href="https://magicwater.uz/"> <img src="assets/images/lang/uz.jpg" style="width: 170%;border-radius: 3px;"> </a></li>
+                            </ul>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+<!-- ...:::: HEADER MENU :::... -->
+
+
+<!-- ...:::: MOBILE MENU SOCIAL :::... -->
+    <div class="mobile-header  mobile-header-bg-color--black section-fluid d-lg-block d-xl-none">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 d-flex align-items-center justify-content-between">
+
+                    <div class="mobile-header-left">
+                        <ul class="mobile-menu-logo">
+                            <li>
+                                <a href="index.html">
+                                    <div class="logo">
+                                        <a href="https://magicwater.uz/ru/"><img src="assets/images/logo/logo.png" alt=""></a>
+                                    </div>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+
+
+                    <div class="mobile-right-side">
+                        <ul class="header-action-link action-color--white action-hover-color--pink">
+                                <li><a href="https://www.facebook.com/magicuzb"><i class="fa fa-facebook"></i></a></li>
+                                <li><a href="https://www.instagram.com/magicwater_uz/"><i class="fa fa-instagram"></i></a></li>
+                                <li><a href="https://t.me/magicwater_uz"><i class="fa fa-telegram"></i></a></li>
+                                <li><a href="https://magicwater.uz/"> <img src="assets/images/lang/uz.jpg" style="width: 170%;border-radius: 3px;"> </a></li>
+                            <li><a href="#mobile-menu-offcanvas" class="offcanvas-toggle offside-menu offside-menu-color--black"><i class="icon-menu"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+<!-- ...:::: MOBILE MENU SOCIAL :::... -->
+
+
+<!-- ...:::: MOBILE MENU OFFCONVAS :::... -->
+    <div id="mobile-menu-offcanvas" class="offcanvas offcanvas-rightside offcanvas-mobile-menu-section">
+
+        <div class="offcanvas-header text-end">
+            <button class="offcanvas-close"><i class="ion-android-close"></i></button>
+        </div> 
+
+        <div class="offcanvas-mobile-menu-wrapper">
+
+            <div class="mobile-contact-info">
+                
+                <div class="logo">
+                    <a href="https://magicwater.uz/"><img src="assets/images/logo/logo.png" alt=""></a>
+                </div>
+
+                <address class="address">
+                    <span> НАШ АДРЕСС: *** </span>
+                    <span> ЗАКАЗАТЬ: +998 71 203-67-67 </span>
+                    <span> ПОЧТА: inbox@magicwater.uz </span>
+                </address>
+
+                <ul class="social-link">
+                    <li><a href="https://www.facebook.com/magicuzb"><i class="fa fa-facebook"></i></a></li>
+                    <li><a href="https://www.instagram.com/magicwater_uz/"><i class="fa fa-instagram"></i></a></li>
+                    <li><a href="https://t.me/magicwater_uz"><i class="fa fa-telegram"></i></a></li>
+                    <li><a href="https://magicwater.uz/"> <img src="assets/images/lang/uz.jpg" style="width: 50%;border-radius: 3px;"> </a></li>
+                </ul>
+
+                <ul class="user-link">
+                    <li class="has-dropdown"><a href="https://magicwater.uz/ru/" class="active main-menu-link" > ГЛАВНАЯ СТРАНИЦА </a></li>
+                    <li class="has-dropdown has-megaitem"><a href="https://magicwater.uz/ru/shop.php"> НАШ ПРОДУКТЫ </a></li>
+                    <li><a href="https://magicwater.uz/ru/contact.php"> СВЯЗАТЬСЯ С НАМИ </a></li>
+                </ul>
+
+            </div>
+        </div>
+    </div>
+<!-- ...:::: MOBILE MENU OFFCONVAS :::... -->
+
+
+<!-- ...:::: SHOP PRODUCT :::... -->
+    <div class="breadcrumb-section breadcrumb-bg-color--golden">
+        <div class="breadcrumb-wrapper">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <h3 class="breadcrumb-title" style="font-weight: 900; color: #153156; padding-bottom: 50px;" > СВЯЖИТЕСЬ С НАМИ И ОСТАВЬТЕ СВОЙ ЗАКАЗЫ </h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- ...::::Start Contact Section:::... -->
+    <div class="contact-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4">
+                    <!-- Start Contact Details -->
+                    <div class="contact-details-wrapper section-top-gap-100" data-aos="fade-up" data-aos-delay="0" style="border-radius: 10px; border: 1px solid #153156; margin-bottom: 20px;">
+                        <div class="contact-details">
+                            <!-- Start Contact Details Single Item -->
+                            <div class="contact-details-single-item">
+                                <div class="contact-details-icon">
+                                    <i class="fa fa-phone"></i>
+                                </div>
+                                <div class="contact-details-content contact-phone">
+                                    <a style="font-weight: 900;color: #153156;" > СВЯЗАТЬСЯ С НАМИ </a>
+                                    <a href="tel:+998712036767">+998 71 203-67-67</a>
+                                </div>
+                            </div> <!-- End Contact Details Single Item -->
+                            <!-- Start Contact Details Single Item -->
+                            <div class="contact-details-single-item">
+                                <div class="contact-details-icon">
+                                    <i class="fa fa-envelope"></i>
+                                </div>
+                                <div class="contact-details-content contact-phone">
+                                    <a style="font-weight: 900;color: #153156;" > НАШ ПОЧТОВЫЙ АДРЕС </a>
+                                    <a href="mailto:inbox@magicwater.uz">inbox@magicwater.uz</a>
+                                    
+                                </div>
+                            </div> <!-- End Contact Details Single Item -->
+                            <!-- Start Contact Details Single Item -->
+                            <div class="contact-details-single-item">
+                                <div class="contact-details-icon">
+                                    <i class="fa fa-map-marker"></i>
+                                </div>
+                                <div class="contact-details-content contact-phone">
+                                    <span style="font-weight: 900;color: #153156;" > НАШ АДРЕСС </span>
+                                </div>
+                            </div> <!-- End Contact Details Single Item -->
+                        </div>
+                        <!-- Start Contact Social Link -->
+                        <div class="contact-social">
+                            <h4 style="font-weight: 900;color: #153156;"> ПОДПИСЫВАЙТЕСЬ НА НАС </h4>
+                            <ul>
+                                <li><a href=""><i class="fa fa-facebook"></i></a></li>
+                                <li><a href=""><i class="fa fa-instagram"></i></a></li>
+                                <li><a href=""><i class="fa fa-telegram"></i></a></li>
+                                <li><a href=""><i class="fa fa-telegram"></i></a></li>
+                            </ul>
+                        </div> <!-- End Contact Social Link -->
+                    </div> <!-- End Contact Details -->
+                </div>
+                <div class="col-lg-8">
+                    <div class="contact-form section-top-gap-100" data-aos="fade-up" data-aos-delay="200" style="border-radius: 10px;border: 1px solid #153156;">
+                        <form id="contact-form" action="https://whizthemes.com/mail-php/jaber/contact.php" method="post">
+                            <div class="row">
+                                <div class="col-lg-6">
+                                    <div class="default-form-box mb-20">
+                                        <label for="contact-name" style="font-weight: 900;color: #153156;"> ВАШ ИМЯ: </label>
+                                        <input name="name" type="text" id="contact-name">
+                                    </div>
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="default-form-box mb-20">
+                                        <label for="contact-email" style="font-weight: 900;color: #153156;"> ВАШ ТЕЛЕФОН НОМЕР: </label>
+                                        <input name="name" type="text" id="contact-name">
+                                    </div>
+                                </div>
+                                <div class="col-lg-12">
+                                    <div class="default-form-box mb-20">
+                                        <label for="contact-subject" style="font-weight: 900;color: #153156;"> ВВЕДИТЕ НАЗВАНИЕ ПРОДУКТА: </label>
+                                        <input name="subject" type="text" id="contact-subject">
+                                    </div>
+                                </div>
+                                <div class="col-lg-12">
+                                    <div class="default-form-box mb-20">
+                                        <label for="contact-message" style="font-weight: 900;color: #153156;"> ОСТАВЬТЕ СООБЩЕНИЕ ДЛЯ ВАШЕГО ЗАКАЗА! </label>
+                                        <textarea name="message" id="contact-message" cols="30" rows="10"></textarea>
+                                    </div>
+                                </div>
+                                <div class="col-lg-12">
+                                    <button class="btn btn-lg btn-black-default-hover" type="submit"> ОТПРАВЬ НАМ ... </button>
+                                </div>
+                                <p class="form-messege"></p>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<!-- ...::::ENd Contact Section:::... -->
+
+
+<!-- ...:::: FOOTER :::... -->
+    <footer class="footer-section footer-bg section-top-gap-100" style="margin-top: 70px;">
+        <div class="footer-wrapper">
+            <div class="footer-bottom">
+                <div class="container">
+                    <div class="row justify-content-between align-items-center align-items-center flex-column flex-md-row mb-n6">
+                        
+                        <div class="col-auto mb-6">
+                            <div class="footer-copyright">
+                                <p class="copyright-text"><a href="https://hasthemes.com/"target="_blank"> ВСЕ ПРАВА ЗАЩИЩЕНЫ © 2022 MAGICWATER® - ЧИСТАЯ ПИТЬЕВАЯ ВОДА </a></p>
+                            </div>
+                        </div>
+
+                        <div class="col-auto mb-6">
+                            <div class="footer-payment">
+                                <div class="image">
+                                    <img src="assets/images/payment/payment.png" alt="">
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+<!-- ...:::: FOOTER :::... -->
+
+
+<!-- SCROLLTOP BUTTON -->
+    <button class="material-scrolltop" type="button"></button>
+<!-- SCROLLTOP BUTTON -->
+
+    <script src="assets/js/vendor/vendor.min.js"></script>
+    <script src="assets/js/plugins/plugins.min.js"></script>
+
+    <!-- Main JS -->
+    <script src="assets/js/main.js"></script>
+
+</body>
+</html>
